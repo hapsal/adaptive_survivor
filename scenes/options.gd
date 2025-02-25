2fc6814ca1b38535eb3af40c1530f6771c922ab7
Hannu Salo <hapsal@utu.fi>
2025-02-25 10:22:08 +0200

    feat: added more options

--- scenes/options.gd
+++ scenes/options.gd
@@ -4,10 +4,12 @@ signal options_closed
 signal targeting_mode_changed(mode: int)
 
 @onready var targeting_checkbox = %Auto
+@onready var music_volume = %MusicVolume
 
 func _ready() -> void:
 	targeting_checkbox.toggled.connect(_on_auto_toggled)
 	targeting_checkbox.button_pressed = (GameState.targeting_mode == GameEnums.TargetingMode.AUTO)
+	music_volume.changed.connect(_on_music_volume_value_changed)
 
 func _on_back_pressed() -> void:
 	options_closed.emit()
@@ -19,3 +21,7 @@ func _on_auto_toggled(toggled_on: bool) -> void:
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		_on_back_pressed()
+
+func _on_music_volume_value_changed(value: float) -> void:
+	var wwise_value = value
+	Wwise.set_rtpc_value("MusicVolume", wwise_value, null)
